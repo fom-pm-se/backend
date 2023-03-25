c377af1f67c6e0e3272bbbc00cb46534505527c8
leosch <leon.schoenhoffgw@outlook.com>
2023-03-25 11:00:26 +0100

    #1 Setup spring boot project - add log output
    adds log output on application launch

--- src/main/java/fom/pmse/crms/backend/BackendApplication.java
+++ src/main/java/fom/pmse/crms/backend/BackendApplication.java
@@ -1,12 +1,15 @@
 package fom.pmse.crms.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting BackendApplication");
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
