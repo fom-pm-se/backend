020886515da78a797befb9d1705cd1abfcd3b86a
leosch <leon.schoenhoffgw@outlook.com>
2023-03-28 18:51:32 +0200

    #11 setup swagger ui
    add swagger dependency and add documentation for HealthCheckController

--- src/main/java/fom/pmse/crms/backend/controller/HealthCheckController.java
+++ src/main/java/fom/pmse/crms/backend/controller/HealthCheckController.java
@@ -1,16 +1,19 @@
 package fom.pmse.crms.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(path = "/api/v1/health")
 public class HealthCheckController {
-    @GetMapping()
+    @GetMapping
+    @Operation(summary = "Perform a health check of the backend")
+    @ApiResponse(responseCode = "200", description = "The backend is healthy")
+    @ApiResponse(responseCode = "500", description = "The backend is not healthy")
     public ResponseEntity<String> performHealthCheck() {
         return ResponseEntity.ok("OK");
     }
