b7a7cfcc5b38fdf6f16efff13bdfa841723c3a67
leosch <leon.schoenhoffgw@outlook.com>
2023-03-29 22:25:52 +0200

    #10 update security config
    update security config to fix build

--- src/main/java/fom/pmse/crms/backend/security/config/SecurityConfig.java
+++ src/main/java/fom/pmse/crms/backend/security/config/SecurityConfig.java
@@ -3,16 +3,34 @@ package fom.pmse.crms.backend.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests()
                 .requestMatchers("/**")
                 .permitAll()
-                .and().build();
+                .and()
+                .csrf(disable -> disable.ignoringRequestMatchers("/**"))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
     }
 }
