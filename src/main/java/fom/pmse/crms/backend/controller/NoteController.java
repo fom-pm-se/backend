5fc8d0b9bddf740011044ff25ec347e2bf4a6a70
Leon Schönhoff <49094676+leonschbbht@users.noreply.github.com>
2023-05-07 17:02:27 +0200

    #35 add swagger docs
    add swagger docs for new endpoints

--- src/main/java/fom/pmse/crms/backend/controller/NoteController.java
+++ src/main/java/fom/pmse/crms/backend/controller/NoteController.java
@@ -4,6 +4,8 @@ import fom.pmse.crms.backend.payload.request.CreateNoteDto;
 import fom.pmse.crms.backend.payload.request.UpdateNoteDto;
 import fom.pmse.crms.backend.payload.response.NoteDto;
 import fom.pmse.crms.backend.service.NoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,16 +19,25 @@ import java.util.List;
 public class NoteController {
     private final NoteService noteService;
     @GetMapping("/{partnerId}")
+    @Operation(summary = "Get all notes for a partner")
+    @ApiResponse(responseCode = "200", description = "Notes found")
+    @ApiResponse(responseCode = "400", description = "Partner not found")
     public ResponseEntity<List<NoteDto>> getNotesForPartner(@PathVariable Long partnerId) {
         return ResponseEntity.ok(noteService.findAllNotesByPartnerId(partnerId));
     }
 
     @PostMapping
+    @Operation(summary = "Create a note for a partner")
+    @ApiResponse(responseCode = "200", description = "Note created")
+    @ApiResponse(responseCode = "400", description = "Partner not found")
     public ResponseEntity<NoteDto> createNoteForPartner(@RequestBody CreateNoteDto note) {
         return ResponseEntity.ok(NoteDto.fromNote(noteService.createNoteForPartner(note)));
     }
 
     @PutMapping
+    @Operation(summary = "Update a note")
+    @ApiResponse(responseCode = "200", description = "Note updated")
+    @ApiResponse(responseCode = "400", description = "Note not found")
     public ResponseEntity<NoteDto> updateNote(@RequestBody UpdateNoteDto noteDto) {
         return ResponseEntity.ok(noteService.updateNote(noteDto));
     }
